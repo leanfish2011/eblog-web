29bb210b6f93c1e12e8977b2543eb7248c9deed3
yufei <yangtze_yufei@163.com>
2023-04-05 22:01:32 +0800

    feat: 搜索页

--- src/components/home/SearchHeader.vue
+++ src/components/home/SearchHeader.vue
@@ -16,82 +16,77 @@
             <el-link href="/" type="primary" icon="el-icon-back">回到首页</el-link>
           </el-form>
         </div>
-        <div id="userInfo"></div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "SearchHeader",
-    data() {
-      return {
-        searchForm: {
-          keyword: ''
-        }
+export default {
+  name: "SearchHeader",
+  data() {
+    return {
+      searchForm: {
+        keyword: ''
       }
-    },
-    methods: {
-      search() {
-        let keyword = this.searchForm.keyword.trim();
-        if (keyword == "") {
-          this.$message.warning("请输入关键词！");
-          return;
-        }
-
-        //子组件调用父组件方法
-        this.$emit('search', keyword);
-      }
-    },
-    mounted() {
-      //搜索框显示主页传递过来的关键词
-      let keyword = this.$route.query.keyword;
-      this.searchForm.keyword = keyword;
     }
+  },
+  methods: {
+    search() {
+      let keyword = this.searchForm.keyword.trim();
+      if (keyword == "") {
+        this.$message.warning("请输入关键词！");
+        return;
+      }
+
+      //子组件调用父组件方法
+      this.$emit('search', keyword);
+    }
+  },
+  mounted() {
+    //搜索框显示主页传递过来的关键词
+    let keyword = this.$route.query.keyword;
+    this.searchForm.keyword = keyword;
   }
+}
 </script>
 
 <style scoped>
-  #topMenu {
-    width: 100%;
-    top: 0;
-    background: white;
-    z-index: 100;
-    margin: 0 auto 0 auto;
-    position: fixed;
-  }
+#topMenu {
+  width: 100%;
+  top: 0;
+  background: white;
+  z-index: 100;
+  margin: 0 auto 0 auto;
+  position: fixed;
+}
 
-  #top {
-    border: 1px solid #D8DFEA;
-    margin: 5px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
+#top {
+  border: 1px solid #D8DFEA;
+  margin: 5px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 
-  #log {
-    width: 4%;
-  }
+#log {
+  width: 4%;
+}
 
-  #logTitle {
-    font-size: 25px;
-    color: #E33E06;
-    width: 6%;
-  }
+#logTitle {
+  font-size: 25px;
+  color: #E33E06;
+  width: 10%;
+}
 
-  #midSearch {
-    width: 60%;
-    margin-left: 50px;
-  }
+#midSearch {
+  width: 90%;
+  margin-left: 50px;
+}
 
-  .searchInput {
-    width: 400px;
-    height: 30px;
-    font-size: 15px;
-  }
-
-  #userInfo {
-    width: 30%;
-  }
+.searchInput {
+  width: 400px;
+  height: 30px;
+  font-size: 15px;
+}
 </style>
--- src/components/home/item/Search.vue
+++ src/components/home/item/Search.vue
@@ -3,7 +3,7 @@
     <search-header @search="onSearch"></search-header>
     <div class="searchContent">
       <el-row :gutter="8">
-        <label v-for="item in myDataList" :key="item.id">
+        <label v-for="item in dataList" :key="item.id">
           <el-col :span="4">
             <el-card shadow="hover" class="siteCard">
               <el-row>
@@ -14,9 +14,9 @@
                 </el-col>
                 <el-col :span="18">
                   <el-link :href="item.url" target="_blank" style="font-size: 16px;"
-                           v-html="highlight(item.name)">
+                           v-html="highlight(item.title)">
                   </el-link>
-                  <div class="remark" v-html="highlight(item.remark)"></div>
+                  <div class="remark" v-html="highlight(item.content)"></div>
                 </el-col>
               </el-row>
             </el-card>
@@ -29,96 +29,96 @@
 </template>
 
 <script>
-  import SearchHeader from '../SearchHeader'
-  import Service from '../../../config/service'
-  import Footer from '../Footer'
+import SearchHeader from '../SearchHeader'
+import Service from '../../../config/service'
+import Footer from '../Footer'
 
-  export default {
-    name: 'search',
-    components: {
-      "SearchHeader": SearchHeader,
-      "bottomFooter": Footer
-    },
-    data() {
-      return {
-        myDataList: null,
-        searchForm: {
-          keyword: ''
-        }
-      }
-    },
-    created() {
-      //从url中获取参数
-      let keyword = this.$route.query.keyword.trim();
-
-      this.onSearch(keyword);
-    },
-    methods: {
-      queryIcon(url) {
-        if (url == null || url == "") {
-          return "../../../../static/eblog.ico";
-        }
-
-        return url;
-      },
-      onSearch(keyword) {
-        this.searchForm.keyword = keyword;
-        this.$axios.get(Service.siteUrl.search, {
-          params: this.searchForm
-        }).then((res) => {
-          if (res.status === 200) {
-            let responseData = res.data;
-            if (responseData.code === 0) {
-              this.myDataList = responseData.data;
-            } else {
-              this.$message.error(responseData.msg);
-            }
-          } else {
-            this.$message.error("系统内部错误");
-          }
-        }).catch(function (error) {
-          console.error(error);
-        });
-      },
-      highlight(content) {
-        if (content == null || content == "") {
-          return;
-        }
-
-        let keyword = this.searchForm.keyword;
-        // 匹配关键字正则
-        let replaceReg = new RegExp(keyword, 'g');
-        // 高亮替换v-html值
-        let replaceString = '<span style="color: red;">' + keyword + '</span>';
-        // 开始替换
-        return content.replace(replaceReg, replaceString);
+export default {
+  name: 'search',
+  components: {
+    "SearchHeader": SearchHeader,
+    "bottomFooter": Footer
+  },
+  data() {
+    return {
+      dataList: null,
+      searchForm: {
+        title: ''
       }
     }
+  },
+  created() {
+    //从url中获取参数
+    let keyword = this.$route.query.keyword.trim();
+
+    this.onSearch(keyword);
+  },
+  methods: {
+    queryIcon(url) {
+      if (url == null || url == "") {
+        return "../../../../static/eblog.ico";
+      }
+
+      return url;
+    },
+    onSearch(keyword) {
+      this.searchForm.title = keyword;
+      this.$axios.get(Service.blogUrl.blog, {
+        params: this.searchForm
+      }).then((res) => {
+        if (res.status === 200) {
+          let responseData = res.data;
+          if (responseData.code === 0) {
+            this.dataList = responseData.data.list;
+          } else {
+            this.$message.error(responseData.msg);
+          }
+        } else {
+          this.$message.error("系统内部错误");
+        }
+      }).catch(function (error) {
+        console.error(error);
+      });
+    },
+    highlight(content) {
+      if (content == null || content == "") {
+        return;
+      }
+
+      let keyword = this.searchForm.title;
+      // 匹配关键字正则
+      let replaceReg = new RegExp(keyword, 'g');
+      // 高亮替换v-html值
+      let replaceString = '<span style="color: red;">' + keyword + '</span>';
+      // 开始替换
+      return content.replace(replaceReg, replaceString);
+    }
   }
+}
 </script>
 
 <style scoped>
-  #search {
-    width: 100%;
-    margin-top: 70px;
-  }
+#search {
+  width: 100%;
+  margin-top: 70px;
+}
 
-  .searchContent {
-    width: 99%;
-    min-height: calc(100vh - 136px);
-    margin-left: 10px;
-  }
+.searchContent {
+  width: 99%;
+  min-height: calc(100vh - 136px);
+  margin-left: 10px;
+}
 
-  .siteCard {
-    height: 80px;
-  }
+.siteCard {
+  height: 80px;
+}
 
-  .siteIcon {
-    width: 50%;
-    height: 50%;
-  }
+.siteIcon {
+  width: 50%;
+  height: 50%;
+}
 
-  .remark {
-    font-size: 14px;
-  }
+.remark {
+  font-size: 14px;
+}
 </style>
